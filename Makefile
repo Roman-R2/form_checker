2d93a9aa18c422b22859cd33452e94c262902cfe
Roman <slyusarrv@yandex.ru>
2023-01-08 13:41:53 +0500

    added mongo in docker

--- Makefile
+++ Makefile
@@ -2,7 +2,25 @@ docker-build:
 	docker-compose build
 
 docker-up:
-	docker-compose up
+	docker-compose up -d
+
+docker-build-up:
+	docker-compose up -d --build
 
 docker-down:
 	docker-compose down --remove-orphans
+
+docker-logs:
+	docker-compose -f docker-compose.yml logs
+
+docker-logs-web-app:
+	docker-compose -f docker-compose.yml logs web-app
+
+docker-logs-mongo:
+	docker-compose -f docker-compose.yml logs mongo -f
+
+docker-logs-mongo-express:
+	docker-compose -f docker-compose.yml logs mongo-express -f
+
+web-app-cli:
+	docker exec -ti web-app sh
--- service/service/settings.py
+++ service/service/settings.py
@@ -9,28 +9,35 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environments from .env file
+load_dotenv(BASE_DIR.parent / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v08uuydv($$cihhwd8g1x7ubrzptz5fv8!j09j3h^qa06m06s*'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost ").split(" ")
 
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+
+    # Third-party apps
+
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'service.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -79,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -99,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -112,7 +116,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
